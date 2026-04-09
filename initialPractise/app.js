b4271c0c2c878858dd1ba4007aff67ac702eb147
Akhil chaturvedi <128232661+akhil7895@users.noreply.github.com>
2026-04-09 21:26:57 +0530

    01-folder done

---
+++ initialPractise/app.js
@@ -0,0 +1,8 @@
+import kuchbhi from "../script.js";
+import {h1} from "../script.js";
+
+console.log(kuchbhi);
+
+h1.innerHTML = "Hello From Akhil ....";
+ document.body.appendChild(h1);
+
---
+++ initialPractise/script.js
@@ -0,0 +1,18 @@
+export var h1 = document.createElement("h1");
+
+ const a = 10;
+ export default a;
+
+
+
+// there is a difference between  named export and default export 
+
+ //Libraries (used for perticular one feature):-
+ //GSAP :- for moving animations
+ //Lenis :- smooth scrolling
+ //ReactJS :- for building user interfaces
+
+
+ //FrameWork :-
+    //NextJS :- for server side rendering and static site generation
+    //Angular
